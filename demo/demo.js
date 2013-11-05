742d5b40be707af98fe05714fb9fd7bc10ff36aa
Joel Purra <code+github@joelpurra.com>
2013-11-05 17:44:47 +0100

    Updated demo, but it's still pretty lame

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,19 @@
-bespoke.horizontal.from('article', {
-  convenient: true
-});
+/*global document:true, bespoke:true */
+
+(function(document, bespoke) {
+    "use strict";
+
+    var deck = bespoke.horizontal.from("article", {
+        convenient: true
+    }),
+        goToFirstButton = document.getElementById("go-to-first"),
+        goToLastButton = document.getElementById("go-to-last");
+
+    goToFirstButton.onclick = function() {
+        deck.first();
+    };
+
+    goToLastButton.onclick = function() {
+        deck.last();
+    };
+}(document, bespoke));
