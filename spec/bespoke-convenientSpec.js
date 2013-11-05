0211042299118877be1c40762b23200f025e851c
Joel Purra <code+github@joelpurra.com>
2013-11-05 13:05:54 +0100

    Fully functional code and some tests

--- spec/bespoke-convenientSpec.js
+++ spec/bespoke-convenientSpec.js
@@ -1,35 +1,98 @@
-(function() {
-  'use strict';
+/*global document:true, jasmine:true, bespoke:true, describe:true, it:true, expect:true, beforeEach:true */
 
-  describe("bespoke-convenient", function() {
+(function(document, jasmine, bespoke, describe, it, expect, beforeEach) {
+    "use strict";
 
-    var deck,
+    describe("bespoke-convenient", function() {
 
-      createDeck = function() {
-        var parent = document.createElement('article');
-        for (var i = 0; i < 10; i++) {
-          parent.appendChild(document.createElement('section'));
-        }
+        var deck,
 
-        deck = bespoke.from(parent, {
-          convenient: true
+            createDeck = function() {
+                var parent = document.createElement("article");
+                for (var i = 0; i < 10; i++) {
+                    parent.appendChild(document.createElement("section"));
+                }
+
+                deck = bespoke.from(parent, {
+                    convenient: true
+                });
+            };
+
+        beforeEach(createDeck);
+
+        describe("deck.firstIndex", function() {
+            it("should always return 0", function() {
+                expect(deck.firstIndex()).toBe(0);
+            });
         });
-      };
 
-    beforeEach(createDeck);
+        describe("deck.lastIndex", function() {
+            it("should return the index of the last slide", function() {
+                expect(deck.lastIndex()).toBe(9);
+            });
+        });
 
-    describe("deck.slide", function() {
+        describe("deck.first", function() {
+            beforeEach(function() {
+                deck.slide(5);
+            });
 
-      beforeEach(function() {
-        deck.slide(0);
-      });
+            it("should go to first slide", function() {
+                expect(deck.slides[5].classList.contains("bespoke-active")).toBe(true);
+                deck.first();
+                expect(deck.slides[0].classList.contains("bespoke-active")).toBe(true);
+            });
 
-      it("should not add a useless 'foobar' class to the slide", function() {
-        expect(deck.slides[0].classList.contains('foobar')).toBe(false);
-      });
+            it("should fire first event", function() {
+                var eventListener = jasmine.createSpy("eventListener"),
+                    off = deck.on("convenient.first", eventListener);
+                deck.first();
+                expect(eventListener).toHaveBeenCalled();
+                off();
+            });
+        });
 
+        describe("deck.last", function() {
+            beforeEach(function() {
+                deck.slide(5);
+            });
+
+            it("should go to last slide", function() {
+                expect(deck.slides[5].classList.contains("bespoke-active")).toBe(true);
+                deck.last();
+                expect(deck.slides[9].classList.contains("bespoke-active")).toBe(true);
+            });
+
+            it("should fire last event", function() {
+                var eventListener = jasmine.createSpy("eventListener"),
+                    off = deck.on("convenient.last", eventListener);
+                deck.last();
+                expect(eventListener).toHaveBeenCalled();
+                off();
+            });
+        });
+
+        describe("deck.createEventData", function() {
+            var eventNamespace = "spec",
+                eventName = "createEventData";
+
+            it("should have an index property when initalized with a slide object", function() {
+                var index = 3,
+                    slide = deck.slides[index],
+                    eventData = deck.createEventData(eventNamespace, eventName, null, slide);
+
+                expect(eventData.index).toBe(index);
+                expect(eventData.slide).toBe(slide);
+            });
+
+            it("should have a slide property when initalized with a slide index", function() {
+                var index = 3,
+                    slide = deck.slides[index],
+                    eventData = deck.createEventData(eventNamespace, eventName, null, index);
+
+                expect(eventData.index).toBe(index);
+                expect(eventData.slide).toBe(slide);
+            });
+        });
     });
-
-  });
-
-}());
+}(document, jasmine, bespoke, describe, it, expect, beforeEach));
--- src/bespoke-convenient.js
+++ src/bespoke-convenient.js
@@ -1,41 +1,90 @@
-(function(bespoke) {
+/*global Math:true, bespoke:true */
 
-  bespoke.plugins.convenient = function(deck) {
+(function(Math, bespoke, ns, pluginName, undefined) {
+    "use strict";
 
-    /*
-      Interact with the deck
-      https://github.com/markdalgleish/bespoke.js#deck-instances
-    */
-    deck.next();
-    deck.prev();
-    deck.slide(0);
+    var tag = "bespoke." + pluginName,
 
-    /*
-       Handle events
-       https://github.com/markdalgleish/bespoke.js#events
-    */
-    deck.on('activate', function(e) {
-      console.log('Slide #' + e.index + ' was activated!', e.slide);
-    });
+        generateErrorObject = function(message) {
+            return new Error(tag + ": " + message);
+        },
 
-    deck.on('deactivate', function(e) {
-      console.log('Slide #' + e.index + ' was deactivated!', e.slide);
-    });
+        eventNamespace = pluginName,
 
-    deck.on('next', function(e) {
-      console.log('The next slide is #' + (e.index + 1), deck.slides[e.index + 1]);
-      // return false to cancel user interaction
-    });
+        eventInNamespace = function(eventName) {
+            return eventNamespace + "." + eventName;
+        },
 
-    deck.on('prev', function(e) {
-      console.log('The previous slide is #' + (e.index - 1), deck.slides[e.index - 1]);
-      // return false to cancel user interaction
-    });
+        plugin = function self(deck) {
+            deck.firstIndex = self.firstIndex.bind(deck);
+            deck.lastIndex = self.lastIndex.bind(deck);
+            deck.first = self.first.bind(deck);
+            deck.last = self.last.bind(deck);
+            deck.createEventData = self.createEventData.bind(deck);
+        },
 
-    deck.on('slide', function(e) {
-      console.log('The requested slide is #' + e.index, e.slide);
-      // return false to cancel user interaction
-    });
-  };
+        isNumber = function(n) {
+            // http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
+            // From http://stackoverflow.com/a/1830844
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        },
 
-}(bespoke));
+        internalFire = function(deck, eventName, innerEvent, index, customData) {
+            return deck.fire(eventInNamespace(eventName), deck.createEventData(eventNamespace, eventName, innerEvent, index, customData));
+        };
+
+    // Plugin functions expect to be executed in a deck context
+    plugin.firstIndex = function() {
+        var index = 0;
+
+        return index;
+    };
+
+    plugin.lastIndex = function() {
+        var index = Math.max(0, this.slides.length - 1);
+
+        return index;
+    };
+
+    plugin.first = function(customData) {
+        var index = this.firstIndex(),
+            result = internalFire(this, "first", null, index, customData) && this.slide(index, customData);
+
+        return result;
+    };
+
+    plugin.last = function(customData) {
+        var index = this.lastIndex(),
+            result = internalFire(this, "last", null, index, customData) && this.slide(index, customData);
+
+        return result;
+    };
+
+    // Mimicing, and extending,the internal createEventData bespoke uses
+    plugin.createEventData = function(eventNamespace, eventName, innerEvent, slide, eventData) {
+        eventData = eventData || {};
+
+        eventData.eventNamespace = eventNamespace || null;
+
+        eventData.eventName = eventName || null;
+
+        // Can be either a DOM/browser event or a bespoke event
+        eventData.innerEvent = innerEvent || null;
+
+        if (isNumber(slide)) {
+            eventData.index = slide;
+            eventData.slide = this.slides[slide];
+        } else {
+            eventData.index = this.slides.indexOf(slide);
+            eventData.slide = slide;
+        }
+
+        return eventData;
+    };
+
+    if (ns[pluginName] !== undefined) {
+        throw generateErrorObject("The " + pluginName + " plugin has already been loaded.");
+    }
+
+    ns[pluginName] = plugin;
+}(Math, bespoke, bespoke.plugins, "convenient"));
